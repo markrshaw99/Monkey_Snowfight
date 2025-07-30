d40a0ce0f983e5a290542b1d9e3e7a86890b7773
markrshaw99 <markshaw1999@gmail.com>
2025-07-30 16:33:41 +0100

    Set up the Users app

--- monkeyusers/apps.py
+++ monkeyusers/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MonkeyusersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'monkeyusers'
+
+    def ready(self):
+        import monkeyusers.signals
--- monkeyusers/models.py
+++ monkeyusers/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = CloudinaryField('image', null=True, blank=True)
+    displayname = models.CharField(max_length=20, null=True, blank=True)
+    info = models.TextField(null=True, blank=True) 
+    
+    def __str__(self):
+        return str(self.user)
+    
+    @property
+    def name(self):
+        if self.displayname:
+            return self.displayname
+        return self.user.username 
+    
+    @property
+    def avatar(self):
+        if self.image:
+            return self.image.url
+        return f'{settings.STATIC_URL}images/avatar.svg'
