b0a1236e74ab5cb5af1f457f5b9cd1e02152c59b
markrshaw99 <markshaw1999@gmail.com>
2025-07-30 15:46:42 +0100

    Initial Commit with Django framework in place

---
+++ monkeygame/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MonkeygameConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'monkeygame'
---
+++ monkeyhome/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MonkeyhomeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'monkeyhome'
